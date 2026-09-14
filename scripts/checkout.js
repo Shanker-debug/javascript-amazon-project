d3c1c721ed68cddc273f68bbaaa2b2cfe8e91457
shanker <kuntagowrishanker685@gmail.com>
2026-09-14 16:48:02 +0530

    convert promises into async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,9 +5,27 @@ import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 
 
+async function loadPage() {
+
+    await loadProductsFetch();
+
+    await new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+
+    renderOrderSummary();
+    renderPaymentSummary();
+}
+
+loadPage();
+
+
+/*
 Promise.all([
     loadProductsFetch(),
-    
+
     new Promise((resolve) => {
         loadCart(() => {
             resolve();
@@ -18,6 +36,7 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
 
 
 /*
